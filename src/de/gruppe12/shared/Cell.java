7c0e1752ac8ac58e36b990a4e6ec633b7d759161
julian <julian.kipka@googlemail.de>
2011-11-21 13:18:05 +0100

    Lens hat die Blöde Blöde Cell geschrieben

---
+++ src/de/gruppe12/shared/Cell.java
@@ -0,0 +1,9 @@
+package de.gruppe12.shared;
+
+import de.fhhannover.inform.hnefatafl.vorgaben.*;
+
+public  interface Cell{
+	public int getRow();
+	public int getCol();
+	public BoardContent getContent();
+}
