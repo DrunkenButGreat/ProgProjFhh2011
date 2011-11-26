ef9dfd1e4a4920ea21011d5be8acebbd6e66c617
blum87 <markus.blume@stud.fh-hannover.de>
2011-11-26 20:40:57 +0100

    wieder push probleme - test

--- src/de/gruppe12/ki/NormalStrategy.java
+++ src/de/gruppe12/ki/NormalStrategy.java
@@ -8,8 +8,8 @@ import de.gruppe12.shared.*;
 * Copyright: (c) 2011 <p>
 * Company: Gruppe 12 <p>
 * @author Markus
-* @version 0.0.2 23.11.2011
-* Änderungen: 23.11. Problem mit Vorgaben.MoveStrategy / Kommentare in calculate
+* @version 0.0.3 23.11.2011
+* Änderungen: 24.11. 2Methoden für calculate Berechnung + Erweiterung von calculate
 */
 
 public class NormalStrategy implements MoveStrategy {
@@ -30,37 +30,75 @@ public class NormalStrategy implements MoveStrategy {
 		return name;
 	}
 
+	//ein erster Versuch, noch nicht ganz der MinMax Algo. Müsste aber fürs Erste reichen.
 	public Move calculateDefenderMove(Move lastMove, int thinktimeInSeconds) {
 		//Speichern in Verlaufbaum, muss noch durchdacht werden
 		//verlauf.setLeft(new Node<Move>(lastMove));
 		
 		//Abbruchbedingung Zeit muss eingefügt werden
 		//if(time>= thinktimeInSeconds){
-		
-		//Hauptcode zur Berechnung nächster Schritt
-		//Platzhalter:
-		Move newM = new Move(lastMove.getToCell(),new Cell(2,3, null));
-		
+		//zeit abgelaufen -> Spiel vorbei ?
 		//} else {
-		return newM;
-		//}
+		//Hauptcode zur Berechnung nächster Schritt
+	    Move[] moves = GenerateMoves();
+	    Move move;
+	    Move best_move=moves[0];
+	    for(int i=0; i<=moves.length; i++) {
+	       move = moves[i];
+	       if (calculateValue(move) > calculateValue(best_move)) {
+	          best_move = move;
+	       }
+	    }
+	    return best_move;
+		//}	
 	}
+	
 
+	
 	public Move calculateAttackerMove(Move lastMove, int thinktimeInSeconds) {
 		//Speichern in Verlaufbaum, muss noch durchdacht werden
 		//verlauf.setRight(new Node<Move>(lastMove));
 		
 		//Abbruchbedingung Zeit muss eingefügt werden
 		//if(time>= thinktimeInSeconds){
-		
-		//Hauptcode zur Berechnung nächster Schritt
-		//Platzhalter:
-		Move newM = new Move(lastMove.getToCell(),new Cell(2,3, null));
-		
+		//zeit abgelaufen -> Spiel vorbei ?
 		//} else {
-		return newM;
+		//Hauptcode zur Berechnung nächster Schritt
+	    Move[] moves = GenerateMoves();
+	    Move move;
+	    Move best_move=moves[0];
+	    for(int i=1; i<=moves.length; i++) {
+	       move = moves[i];
+	       if (calculateValue(move) > calculateValue(best_move)) {
+	          best_move = move;
+	       }
+	    }
+	    return best_move;
 		//}	
 		}
+	
+	
+	/**
+	 * 
+	 * GenerateMoves()
+	 * 
+	 * @return ein Array mit allen Möglichen Moves
+	 */
+	//muss noch implementiert werden
+	private Move[] GenerateMoves() {
+		return null;
+	}
+
+	/**
+	 * calculateValue
+	 * 
+	 * @return Bewertung des Moves
+	 */
+	   private int calculateValue(Move m) {
+		      int value=0;
+		     //implementierung von Bewertung eines Moves fehlt hir noch
+		      return value;
+		   }
 	
 	public String toString(){
 	return name+" Strategy, Gruppe"+getNr;	
