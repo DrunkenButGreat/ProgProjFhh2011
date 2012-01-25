e89364d8747ae2153c54f56c37e591006edc65f4
Julian Kipka <julian.kipka@googlemail.com>
2012-01-25 20:36:17 +0100

    Ah ich hoffe es geht jetzt. Andere Branch aber hier Finale Version
    Signed-off-by: Julian Kipka <julian.kipka@googlemail.com>

--- src/de/gruppe12/gui/GuiController.java
+++ src/de/gruppe12/gui/GuiController.java
@@ -25,7 +25,6 @@ public class GuiController implements Observer {
 	private String lastMoveLog = null;
 	private BoardContent[][] board = null;
 	private boolean kiFailed;
-	private boolean bannerOff;
 
 	public GuiController() {
 		anim = new MoveAnimation(this);
@@ -35,10 +34,6 @@ public class GuiController implements Observer {
 		this.gui = gui;
 	}
 
-	protected void setBannerOff() {
-		bannerOff= true;
-		update();
-	}
 	protected String addKiPathName(String name) {
 		String shortName = name.substring(name.lastIndexOf('\\') + 1);
 		if (shortName.contains("."))
@@ -69,7 +64,6 @@ public class GuiController implements Observer {
 	private void reinit() {
 		kiFailed= false;
 		board= null;
-		bannerOff= false;
 	}
 
 	public void resetLogic() {
@@ -377,10 +371,6 @@ public class GuiController implements Observer {
 		synchronized (logic) {
 			logic.notifyAll();
 		}
-	}
-
-	public boolean bannerIsOff() {
-		return bannerOff;
 	}
 
 }
--- src/de/gruppe12/gui/JPanelBoardDisplay.java
+++ src/de/gruppe12/gui/JPanelBoardDisplay.java
@@ -75,7 +75,6 @@ public class JPanelBoardDisplay extends JPanel {
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				if (gc.gameFinished()) gc.setBannerOff();
 				if (!gc.logicAwaitsPlayerMove()
 						|| gc.getAnimation().isRunning() || gc.gameFinished())
 					return;
@@ -182,31 +181,25 @@ public class JPanelBoardDisplay extends JPanel {
 		}
 
 		if (gc.gameFinished() && !gc.getAnimation().isRunning()) {
-			tempg.setColor(new Color(0,0,0,0.7f));
-			tempg.fillRect(boardStartX-fieldSize, boardStartY-fieldSize, fieldSize*17, fieldSize*17);
-			
-			
-			if (!gc.bannerIsOff()) {
-				Image banner;
-				if (gc.kiDisqualified()) {
-					if (gc.defenderWon()) banner= kiFailDefWinImage;
-					else banner= kiFailOffWinImage;
-				} else {
-					if (gc.defenderWon()) banner= defenderWinImage;
-					else banner= offenderWinImage;
-				}
-	
-				double imageRatio = banner.getHeight(null)
-						/ (double) banner.getWidth(null);
-				double bannerHeight = imageRatio * 19 * fieldSize;
-	
-				tempg.drawImage(
-						banner,
-						boardStartX - 2 * fieldSize,
-						(int) Math.round(boardStartY + 7.5 * fieldSize
-								- bannerHeight / 2), 19 * fieldSize,
-						(int) Math.round(bannerHeight), null);
+			Image banner;
+			if (gc.kiDisqualified()) {
+				if (gc.defenderWon()) banner= kiFailDefWinImage;
+				else banner= kiFailOffWinImage;
+			} else {
+				if (gc.defenderWon()) banner= defenderWinImage;
+				else banner= offenderWinImage;
 			}
+
+			double imageRatio = banner.getHeight(null)
+					/ (double) banner.getWidth(null);
+			double bannerHeight = imageRatio * 19 * fieldSize;
+
+			tempg.drawImage(
+					banner,
+					boardStartX - 2 * fieldSize,
+					(int) Math.round(boardStartY + 7.5 * fieldSize
+							- bannerHeight / 2), 19 * fieldSize,
+					(int) Math.round(bannerHeight), null);
 		}
 
 		tempg.dispose();
